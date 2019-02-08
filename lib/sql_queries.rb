0fcacba1e8d87a3b6179e45d186431e1031e8e9c
Sara Walker <sarawall1187@gmail.com>
2019-02-08 19:35:13 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears MAX;"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = NULL;"
+  "SELECT * FROM bears WHERE bears.name IS NULL;"
 end
