e1db97f4dd726a34a046098ad656fe36ce482432
Sara Walker <sarawall1187@gmail.com>
2019-02-08 18:05:21 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "gentle", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "male", "dark brown", "gentle", 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "male", "black", "rowdy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "female", "brown", "gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "female", "dark brown", "gentle", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "fiesty", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "female", "black", "motherly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "female", "dark brown", "goofy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "male", "brown", "grumpy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "female", "brown", "gentle", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "male", "brown", "aggressive", 0);
